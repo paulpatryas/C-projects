3ad82b5676bfdb0708378d34097bbc18e178e967
paulpatryas <32423930+paulpatryas@users.noreply.github.com>
2017-10-01 19:22:15 -0400

    FizzBuzz - old project

---
+++ FizzBuzz.c
@@ -0,0 +1,25 @@
+/* By: Paul Patryas
+   Infamous FizzBuzz program */
+
+#include <stdio.h>
+
+int main() {
+
+    for(int i=1; i < 100; i++) {
+        // if divisible by 3 & 5, print FizzBuzz
+        if(i % 5 == 0 && i % 3 == 0) {
+            printf("%d is FizzBuzz\n", i);
+        }
+        // if divisible by 5, print Buzz
+        else if(i % 5 == 0) {
+            printf("%d is Buzz\n", i);
+        }
+        // if divisible by 3, print Fizz
+        else if(i % 3 == 0) {
+            printf("%d is Fizz\n", i);
+        }
+    }
+
+    getchar();
+    return 0;
+}
