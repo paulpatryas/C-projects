fdb767ffde9c20a7819334914190c3f89b173db5
paulpatryas <32423930+paulpatryas@users.noreply.github.com>
2017-10-03 18:05:51 -0400

    refactor gone mad
    functions galore

--- src/misc_proj4.c
+++ src/misc_proj4.c
@@ -1,5 +1,5 @@
- /* Name: Paul Patryas
-   Another misc project  
+/* Name: Paul Patryas
+   Another misc project
    Date: 24 Sep 2017 */
 
 #include<stdio.h>
@@ -10,44 +10,15 @@ void cleanInput();
 // declare functions that takes pointers as parameters
 float checkForNonNumericCharacters(int *a, float *b, char *c);
 float checkPostiveNumbersWithBound(int *a, float *b, char *c);
+void checkObservedDays();
+void loopObservedDays(float *b);
+void loopHowManyTrips(float *b);
+void printResult(float *a, float *b);
 
 int main() {
 
-    // variable declarations
-    int i, j, flag, day_counter = 0;
-    char buffer;
-    float observed_days, how_many_trips, trip_length, total, average;
 
-    printf("How many days will you observe the landing crew?\n");
-
-    // initialize flag and assign it to the scanf function
-    // store possible non numeric characters inputted by user
-    flag = scanf("%f%c", &observed_days, &buffer);
-    checkForNonNumericCharacters(&flag, &observed_days, &buffer);
-    checkPostiveNumbersWithBound(&flag, &observed_days, &buffer);
-
-    // out of do...while loop and into for loops
-    for (i=0; i < observed_days; i++) {
-        printf("\nHow many trips were completed in day #%d?\n", i+1);
-        flag = scanf("%f%c", &how_many_trips, &buffer);
-        checkForNonNumericCharacters(&flag, &how_many_trips, &buffer);
-        checkPostiveNumbersWithBound(&flag, &how_many_trips, &buffer);
-
-            // for loop asking for each trip_length and adding to total
-            for (j=0; j < how_many_trips; j++) {
-                printf("How long was trip #%d in hours?\n", j+1);
-                flag = scanf("%f%c", &trip_length, &buffer);
-                checkForNonNumericCharacters(&flag, &trip_length, &buffer);
-                checkPostiveNumbersWithBound(&flag, &trip_length, &buffer);
-                total += trip_length;
-            }
-        average = total / how_many_trips;
-        printf("\nDay #%d: The average time was %.3f hours.\n", day_counter + 1, average);
-        total = 0;
-        day_counter++;
-
-    }
-
+    checkObservedDays();
 
     return 0;
 
@@ -57,6 +28,51 @@ int main() {
 void cleanInput(void) {
     while (getchar()!='\n');
     return;
+}
+void checkObservedDays(){
+    printf("How many days will you observe the landing crew?\n");
+    int observed_days;
+    char buffer;
+    // initialize flag and assign it to the scanf function
+    // store possible non numeric characters inputted by user
+    int flag = scanf("%f%c", &observed_days, &buffer);
+    checkForNonNumericCharacters(&flag, &observed_days, &buffer);
+    checkPostiveNumbersWithBound(&flag, &observed_days, &buffer);
+    loopObservedDays(&observed_days);
+}
+
+void loopObservedDays(float *b){
+    float how_many_trips;
+    char buffer;
+    // out of do...while loop and into for loops
+    for (int i = 0; i < *b; i++) {
+        printf("\nHow many trips were completed in day #%d?\n", i+1);
+        int flag = scanf("%f%c", &how_many_trips, &buffer);
+        checkForNonNumericCharacters(&flag, &how_many_trips, &buffer);
+        checkPostiveNumbersWithBound(&flag, &how_many_trips, &buffer);
+        loopHowManyTrips(&how_many_trips);
+    }
+
+}
+
+void loopHowManyTrips(float *b) {
+    float trip_length, total, total_sum;
+    char buffer;
+
+    for (int j=0; j < *b; j++) {
+        printf("How long was trip #%d in hours?\n", j+1);
+        int flag = scanf("%f%c", &trip_length, &buffer);
+        checkForNonNumericCharacters(&flag, &trip_length, &buffer);
+        checkPostiveNumbersWithBound(&flag, &trip_length, &buffer);
+        total += trip_length;
+    }
+    printResult(&total, &*b);
+}
+
+void printResult(float *a, float *b) {
+    float average = *a / *b;
+    printf("\nThe average time was %.2f hours.\n", average);
+    *a = 0;
 }
 
 // function that takes pointers as parameters
