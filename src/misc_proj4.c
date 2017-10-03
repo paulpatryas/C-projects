9dedbbfbc0f50255bac4caa3ff914e355656276d
paulpatryas <32423930+paulpatryas@users.noreply.github.com>
2017-10-03 14:46:38 -0400

    slight changes

--- src/misc_proj4.c
+++ src/misc_proj4.c
@@ -2,16 +2,13 @@
    Another misc project
    Date: 24 Sep 2017 */
 
-// preprocessor directive
 #include<stdio.h>
 
 // declare cleanup function
 void cleanInput();
 
-// declare function that takes pointers as parameters
+// declare functions that takes pointers as parameters
 float checkForNonNumericCharacters(int *a, float *b, char *c);
-
-// declare function that takes pointers as parameters
 float checkPostiveNumbersWithBound(int *a, float *b, char *c);
 
 int main() {
@@ -26,40 +23,30 @@ int main() {
     // initialize flag and assign it to the scanf function
     // store possible non numeric characters inputted by user
     flag = scanf("%f%c", &observed_days, &buffer);
-
-    // pass arguments to function
     checkForNonNumericCharacters(&flag, &observed_days, &buffer);
     checkPostiveNumbersWithBound(&flag, &observed_days, &buffer);
-
 
     // out of do...while loop and into for loops
     for (i=0; i < observed_days; i++) {
         printf("\nHow many trips were completed in day #%d?\n", i+1);
-        // store flag as input for next for loop and check_input function
         flag = scanf("%f%c", &how_many_trips, &buffer);
-        // pass arguments to function
         checkForNonNumericCharacters(&flag, &how_many_trips, &buffer);
         checkPostiveNumbersWithBound(&flag, &how_many_trips, &buffer);
 
             // for loop asking for each trip_length and adding to total
             for (j=0; j < how_many_trips; j++) {
                 printf("How long was trip #%d in hours?\n", j+1);
-                // store flag as input for check_input function
                 flag = scanf("%f%c", &trip_length, &buffer);
-                // pass arguments to function
                 checkForNonNumericCharacters(&flag, &trip_length, &buffer);
                 checkPostiveNumbersWithBound(&flag, &trip_length, &buffer);
-                // total = total + trip_length
                 total += trip_length;
             }
-        // once out of 2nd for loop, do simple calculations for average and go back to 1st for loop
         average = total / how_many_trips;
-        // print out average for each Day#, reset total to zero, and increment day_counter by 1
         printf("\nDay #%d: The average time was %.3f hours.\n", day_counter + 1, average);
         total = 0;
         day_counter++;
 
-    } // run main for loop until false and then quit program
+    }
 
 
     return 0;
@@ -75,35 +62,25 @@ void cleanInput(void) {
 // function that takes pointers as parameters
 float checkForNonNumericCharacters(int *a, float *b, char *c){
     do {
-        // checks user input for non-numeric characters or a mix of numeric and non-numeric characters
         if (*a != 2 || *c != '\n'){
-            // prints out an error message for the user to enter a positive number
             printf("Input error of non numeric characters. Please enter a positive number\n");
-            // cleans input from terminal or else the do...while loop will run indefinitely for non numeric characters entered
             cleanInput();
-            // stores user input as a flag
             *a = scanf("%f%c", &*b, &*c);
+            return *b;
         }
         else
             break;
        }while(1); // continue do...while loop until false
-
-    return *b; // return float value *this is the input the user entered for the question*}
 }
 
 // function that takes pointers as parameters
 float checkPostiveNumbersWithBound(int *a, float *b, char *c){
-    // checks user input if it is in range of defined bound of 0 and 10, if not, ask user to enter correct lower bound
     while(!(0 <= *b && *b <= 10)){
         printf("Please enter a positive number between 0 and 10\n");
-        // stores user input as a flag
-        *a = scanf("%f%c", &*b, *&c);
-        // pass arguments to function and check for non-numeric characters again
+        *a = scanf("%f%c", &*b, *&c)'
         checkForNonNumericCharacters(&*a,&*b,&*c);
+        return *b;
     }
-    // return float value *this is the input the user entered for the question*
-    return *b;
 }
-
 
 
